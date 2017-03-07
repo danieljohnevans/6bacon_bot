076f132ba1b1505504086aaeffdf50778451ced2
JR Ladd <johnrobertladd@gmail.com>
2017-03-07 13:52:09 -0500

    started compose_tweet function

--- randomizer.py
+++ randomizer.py
@@ -7,25 +7,49 @@ from secrets import *
 from random import randint
 import json
 from urllib.request import urlopen
+#import tweepy
 
-
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+#auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+#auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+#api = tweepy.API(auth)
 
 randos = randint(100000000,100200719);
 
 def find_by_rel(id):
     return "http://www.sixdegreesoffrancisbacon.com/relationships/{}.json".format(id)
 
-def find_by_ppl(numb):
-    print("http://www.sixdegreesoffrancisbacon.com/people/{}.json".format(numb))
+def find_by_ppl(id):
+    return "http://www.sixdegreesoffrancisbacon.com/people/{}.json".format(id)
 
+def compose_tweet(id):
+    url = find_by_rel(randos)
+    data = json.loads(list(response)[0].decode('utf-8'))
+    relationship_data = data["type_certainty_list"]
+    if len(relationship_data) > 1:
+        relationship_data = relationship_data[-1]
+    else:
+        relationship_data = relationship_data[0]
 
-url = find_by_rel(randos)
+    conf = relationship_data[1]
+    reltype = relationship_data[2]
+    person1_id = data['person1_index']
+    person2_id = data['person2_index']
+
+    
+def parse_confidence(conf):
+    if conf >= 80:
+        return 'certain'
+    elif conf < 80 and conf >= 60:
+        return 'very likely'
+    elif conf < 60 and conf >= 40:
+        return 'possible'
+    elif conf < 40 and conf >= 20:
+        return 'unlikely'
+    else:
+        return 'very unlikely'
+
 print(url)
 response = urlopen(url)
-data = json.loads(list(response)[0].decode('utf-8'))
 print(data)
-find_by_ppl(randos)
+person = find_by_ppl(randos)
+print(person)
