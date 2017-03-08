a4a533c27a5b4e1e22bb953c692d1486d3f8862d
JR Ladd <johnrobertladd@gmail.com>
2017-03-08 14:15:38 -0500

    compose basic tweet and update timeline

--- randomizer.py
+++ randomizer.py
@@ -7,11 +7,11 @@ from secrets import *
 from random import randint
 import json
 from urllib.request import urlopen
-#import tweepy
+import tweepy
 
-#auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-#auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-#api = tweepy.API(auth)
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
 randos = randint(100000000,100200719);
 
@@ -21,20 +21,12 @@ def find_by_rel(id):
 def find_by_ppl(id):
     return "http://www.sixdegreesoffrancisbacon.com/people/{}.json".format(id)
 
-def compose_tweet(id):
-    url = find_by_rel(randos)
+def get_person_name(id):
+    url = find_by_ppl(id)
+    response = urlopen(url)
     data = json.loads(list(response)[0].decode('utf-8'))
-    relationship_data = data["type_certainty_list"]
-    if len(relationship_data) > 1:
-        relationship_data = relationship_data[-1]
-    else:
-        relationship_data = relationship_data[0]
-
-    conf = relationship_data[1]
-    reltype = relationship_data[2]
-    person1_id = data['person1_index']
-    person2_id = data['person2_index']
-
+    name = data['display_name']
+    return name
     
 def parse_confidence(conf):
     if conf >= 80:
@@ -48,8 +40,32 @@ def parse_confidence(conf):
     else:
         return 'very unlikely'
 
-print(url)
-response = urlopen(url)
-print(data)
-person = find_by_ppl(randos)
-print(person)
+def compose_tweet(id):
+    url = None
+    while url == None:
+        try:
+            url = find_by_rel(randos)
+        except HTTPError:
+            pass
+    response = urlopen(url)
+    data = json.loads(list(response)[0].decode('utf-8'))
+    relationship_data = data["type_certainty_list"]
+    if len(relationship_data) > 1:
+        relationship_data = relationship_data[-1]
+    else:
+        relationship_data = relationship_data[0]
+
+    conf = relationship_data[1]
+    confidence = parse_confidence(conf)
+    reltype = relationship_data[2]
+    person1_id = data['person1_index']
+    person1_name = get_person_name(person1_id)
+    person2_id = data['person2_index']
+    person2_name = get_person_name(person2_id)
+    tweet = "It is {0} that {1} {2} {3}. See their shared network: http://sixdegreesoffrancisbacon.com/?id={4}&id2={5}&confidence=60,100&date=1500,1700&table=no".format(confidence, person1_name, reltype, person2_name, person1_id, person2_id)
+    return tweet
+
+
+tweet = compose_tweet(randos)
+api.update_status(tweet)
+
