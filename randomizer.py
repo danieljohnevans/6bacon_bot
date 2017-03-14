aa164decdc35e76b1d2732e7d809ec0b4715e92e
JR Ladd <johnrobertladd@gmail.com>
2017-03-14 15:40:42 -0400

    randomize person or relationship tweet, add gitignore, handle errors and yeartype

--- randomizer.py
+++ randomizer.py
@@ -4,16 +4,17 @@
 
 
 from secrets import *
-from random import randint
+from random import randint, choice
 import json
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from json.decoder import JSONDecodeError
 import tweepy
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-randos = randint(100000000,100200719);
 
 def find_by_rel(id):
     return "http://www.sixdegreesoffrancisbacon.com/relationships/{}.json".format(id)
@@ -27,7 +28,7 @@ def get_person_name(id):
     data = json.loads(list(response)[0].decode('utf-8'))
     name = data['display_name']
     return name
-    
+
 def parse_confidence(conf):
     if conf >= 80:
         return 'certain'
@@ -40,14 +41,15 @@ def parse_confidence(conf):
     else:
         return 'very unlikely'
 
-def compose_tweet(id):
-    url = None
-    while url == None:
+def compose_rel_tweet():
+    response = None
+    while response == None:
         try:
-            url = find_by_rel(randos)
+            id = randint(100000000,100200719)
+            url = find_by_rel(id)
+            response = urlopen(url)
         except HTTPError:
             pass
-    response = urlopen(url)
     data = json.loads(list(response)[0].decode('utf-8'))
     relationship_data = data["type_certainty_list"]
     if len(relationship_data) > 1:
@@ -55,17 +57,55 @@ def compose_tweet(id):
     else:
         relationship_data = relationship_data[0]
 
-    conf = relationship_data[1]
+    conf = int(relationship_data[1])
     confidence = parse_confidence(conf)
     reltype = relationship_data[2]
     person1_id = data['person1_index']
     person1_name = get_person_name(person1_id)
     person2_id = data['person2_index']
     person2_name = get_person_name(person2_id)
-    tweet = "It is {0} that {1} {2} {3}. See their shared network: http://sixdegreesoffrancisbacon.com/?id={4}&id2={5}&confidence=60,100&date=1500,1700&table=no".format(confidence, person1_name, reltype, person2_name, person1_id, person2_id)
+    tweet = "6Bacon is {0}% certain that {1} {2} {3}. See their shared network: http://sixdegreesoffrancisbacon.com/?id={4}&id2={5}&confidence={0},100&date=1500,1700&table=no".format(conf, person1_name, reltype, person2_name, person1_id, person2_id)
     return tweet
 
+def compose_person_tweet():
+    response = None
+    data = None
+    while response == None or data == None:
+        try:
+            id = randint(10000001, 10054877)
+            url = find_by_ppl(id)
+            response = urlopen(url)
+            data = json.loads(list(response)[0].decode('utf-8'))
+        except (HTTPError, JSONDecodeError) as e:
+            pass
+    name = data['display_name'].strip()
+    birthtype = parse_year_type(data['birth_year_type'])
+    birthyear = data['ext_birth_year']
+    deathtype = parse_year_type(data['death_year_type'])
+    deathyear = data['ext_death_year']
+    hist_sig = data['historical_significance'].strip()
+    tweet = "{0}, {1}, was born {2} {3} and died {4} {5}. See the network: http://sixdegreesoffrancisbacon.com/people/{6}".format(name, hist_sig, birthtype, birthyear, deathtype, deathyear, id)
+    return tweet
 
-tweet = compose_tweet(randos)
-api.update_status(tweet)
+def parse_year_type(ytype):
+    if ytype == 'IN':
+        return 'in'
+    elif ytype == 'BF':
+        return 'before'
+    elif ytype == 'AF':
+        return 'after'
+    elif ytype == 'CA':
+        return 'circa'
+    elif ytype == 'BF/IN':
+        return 'before or in'
+    elif ytype == 'AF/IN':
+        return 'after or in'
 
+coinflip = choice([0,1])
+if coinflip == 0:
+    tweet = compose_rel_tweet()
+else:
+    tweet = compose_person_tweet()
+
+# api.update_status(tweet)
+print(tweet)
